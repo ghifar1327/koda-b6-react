c0e91839016fdd1938cf05b6b5d0ceb2a2dee589
ghifar1327 <givartorreto@gmail.com>
2026-01-23 01:20:22 +0700

    init: install lucid react 'icon'

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -7,7 +7,8 @@ export function Button({
   imgSize,
   children,
   size,
-  border
+  border,
+  icon : Icon
 }) {
   console.log(!size)
   return (
@@ -15,6 +16,7 @@ export function Button({
       onClick={onClick}
       className={`${orange && "bg-[#FF8906]"} ${shadow && "shadow-md border border-[#DEDEDE]/10"} ${size ? size : "w-full p-2"} ${border} rounded-md flex items-center justify-center gap-5 cursor-pointer`}
     >
+      {Icon && <Icon/>}
       {src && <img src={src} alt={alt} className={imgSize} />}
       <p>{children}</p>
     </button>
