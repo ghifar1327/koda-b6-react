8b7dd80c2a540c80aee14c0c8c1befba44599f61
ghifar1327 <givartorreto@gmail.com>
2026-02-02 22:31:33 +0700

    feat: create Dashboard page for admin role

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <main className='text-9xl font-bold flex items-center justify-center w-full h-screen'> 
+        <h1>ADMIN</h1>
+    </main>
+  )
+}
