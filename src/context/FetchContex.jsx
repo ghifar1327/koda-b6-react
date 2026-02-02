67ce7761497f57431a2c60e3dda2b6e95e9f2ac3
ghifar1327 <givartorreto@gmail.com>
2026-02-02 19:58:55 +0700

    feat: create a Fetch Context for fetching product data so that it can be used in many components that require the same source

---
+++ src/context/FetchContex.jsx
@@ -0,0 +1,28 @@
+import { createContext, useEffect, useState } from 'react'
+
+const FetchContext = createContext(null)
+
+export  function FetchProvider({children}) {
+    const [products , setProducts] = useState([])
+    
+    useEffect(()=>{
+        (async()=>{
+            const res = await fetch("https://raw.githubusercontent.com/ghifar1327/koda-b6-react/refs/heads/main/products.json")
+            try{
+                if(!res.ok) throw new Error()
+                const data = await res.json()
+                setProducts(data)
+            }catch(err){
+                console.error(err)
+                return
+            }
+        })()
+    },[])
+  
+    return (
+    <FetchContext.Provider value={[products]}>
+        {children}
+    </FetchContext.Provider>
+  )
+}
+export default FetchContext
