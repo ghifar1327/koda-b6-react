7df085368cec6393496b13b693d2f4465cfb9bac
ghifar1327 <givartorreto@gmail.com>
2026-02-02 20:12:54 +0700

    feat: add the FecthProvidor component so that it can be used by components in the router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import Router from "./Router.jsx";
-import {InvoiceProvider} from "./context/InvoiceContext.jsx";
+import { InvoiceProvider } from "./context/InvoiceContext.jsx";
+import { FetchProvider } from "./context/FetchContex.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <InvoiceProvider>
-      <Router />
-    </InvoiceProvider>
+    <FetchProvider>
+      <InvoiceProvider>
+        <Router />
+      </InvoiceProvider>
+    </FetchProvider>
   </StrictMode>,
 );
