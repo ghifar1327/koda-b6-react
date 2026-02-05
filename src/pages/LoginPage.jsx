f615352047d6a55b0dafaf1cf3c45178de1a39de
ghifar1327 <givartorreto@gmail.com>
2026-02-06 03:13:50 +0700

    feat: implementations error and success validate auth

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,32 +4,53 @@ import Input from "../components/common/Input";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Link } from "react-router";
-import { useContext } from "react";
-import  AuthContext  from "../context/AuthContext";
+import { Link, useNavigate } from "react-router";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../context/AuthContext";
+import Modal from "../components/feature/Modal";
 
 const scehema = yup.object({
-  email: yup.string()
+  email: yup
+    .string()
     .email("invalid format email")
     .required("Email must be filled in"),
   password: yup.string().required("password must be filled in"),
 });
 export default function LoginPage() {
-  const {login} = useContext(AuthContext)
+  const navigate = useNavigate();
+  const [count, setCount] = useState(5);
+  const { login, IsSuccess, setIsSuccess, error, setError } =
+    useContext(AuthContext);
   const {
     handleSubmit,
     register,
     reset,
     formState: { errors },
+  } = useForm({ resolver: yupResolver(scehema) });
+  function action(form) {
+    login(form);
   }
- = useForm({ resolver: yupResolver(scehema) });
-function action(form) {
-  login(form)
-  reset()
-}
+  useEffect(() => {
+    setIsSuccess(false);
+  }, []);
+  useEffect(() => {
+    if (IsSuccess) {
+      setError(false);
+      reset();
+      const timer = setInterval(() => {
+        setCount((prev) => {
+          if (prev <= 1) {
+            clearInterval(timer);
+            navigate("/");
+            return 0;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+    }
+  }, [IsSuccess]);
   return (
-    // <AuthLayout src={"/auth1.png"} alt={"login coffie"}>
-      <>
+    <>
       <div>
         <img src="/logos/coffiebrown.png" alt="coffie" />
       </div>
@@ -39,7 +60,11 @@ function action(form) {
       <div className="text-[#4F5665]">
         <p>Fill out the form correctly</p>
       </div>
-      <form action="" className="flex flex-col gap-5" onSubmit={handleSubmit(action)}>
+      <form
+        action=""
+        className="flex flex-col gap-5"
+        onSubmit={handleSubmit(action)}
+      >
         <Input
           label={"Email"}
           type={"text"}
@@ -67,7 +92,10 @@ function action(form) {
       </form>
       <div className="flex justify-center">
         <p>
-          Not Have An Account? <span className="text-primary"><Link to="/register">Register</Link></span>
+          Not Have An Account?{" "}
+          <span className="text-primary">
+            <Link to="/register">Register</Link>
+          </span>
         </p>
       </div>
       <div className="flex justify-between gap-20 items-center text-[#AAAAAA]">
@@ -88,7 +116,30 @@ function action(form) {
           Google
         </Button>
       </div>
-      </>
-    // </AuthLayout>
+      <Modal onClick={() => navigate("/")} success={IsSuccess}>
+        <p className="text-3xl text-gray-700">Login Successful</p>
+        <div className="flex flex-col w-full gap-2">
+          <Button orange onClick={()=> setCount(0)}>Enter</Button>
+          <p className="text-gray-500 text-center">Redirect in {count}s</p>
+        </div>
+      </Modal>
+      <Modal error={error} onClick={() => setError(!error)}>
+        <p className="text-2xl text-gray-700 w-70 text-center">
+          email not registered or password is wrong
+        </p>
+        <div className="flex flex-col gap-3 w-full text-center">
+          <Link
+            to="/register"
+            className="border-2 rounded-md p-2 w-full hover:border-primary hover:text-primary"
+          >
+            Register
+          </Link>
+          <p className="text-gray-500">or</p>
+          <Button orange onClick={() => setError(!error)}>
+            Try Again
+          </Button>
+        </div>
+      </Modal>
+    </>
   );
 }
