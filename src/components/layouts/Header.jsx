b78e7ff2c906e00703660f8dac3bd62decd9be44
ghifar1327 <givartorreto@gmail.com>
2026-02-07 02:44:34 +0700

    feat:Add a modal menu feature that contains user data for navigation to profile and history pages.

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,22 +1,34 @@
-import { Icon, MenuIcon, Search, ShoppingCart } from "lucide-react";
+import { Icon, LogOut, MenuIcon, Search, ShoppingCart } from "lucide-react";
 import { Button } from "../common/Button";
 import MobileMenu from "./MobileMenu";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useLocation } from "react-router";
-import  AuthContext  from "../../context/AuthContext";
+import AuthContext from "../../context/AuthContext";
 
 export default function Header() {
   const { user, logout } = useContext(AuthContext);
   const [toggle, setToggle] = useState(false);
   const location = useLocation();
+
+  useEffect(() => {
+    (()=>{
+      setToggle(false);
+    })()
+}, [location.pathname]);
   function toogleButton(e) {
     e.preventDefault();
     setToggle((prev) => !prev);
   }
   return (
-    <nav
+     user.role === "admin" ?
+      <nav 
+      className={`w-full z-1000 h-auto left-0 top-0  flex justify-between px-[5%] lg:px-[10%] p-3`}
+      >
+      </nav>
+      :
+      <nav
       className={`w-full z-1000 h-auto left-0 top-0 ${location.pathname != "/" ? "bg-black relative" : "bg-[#0B090921] absolute"} text-white flex justify-between px-[5%] lg:px-[10%] p-3`}
-    >
+      >
       <section className="flex items-center gap-3 md:gap-10">
         <img src="/logos/coffieWhite.png" alt="" className="h-8" />
         <div className="hidden md:flex">
@@ -25,12 +37,12 @@ export default function Header() {
         <div className="hidden md:flex">
           <Link to="/product">Product</Link>
         </div>
-      </section>
+        </section>
       <section className="flex items-center gap-5">
         <Link to="">
           <Search size={26} />
         </Link>
-        <Link to={`${user ? "/payment": "/login"}`}>
+        <Link to={`${user ? "/payment" : "/login"}`}>
           <ShoppingCart size={26} />
         </Link>
         <Button onClick={toogleButton} size={"flex md:hidden"}>
@@ -39,14 +51,52 @@ export default function Header() {
         <div className="hidden md:flex gap-5">
           {user ? (
             <div className="flex items-center gap-5">
-              <Button
-                onClick={logout}
-                border={"border border-red-500"}
-                size={"h-10 w-23 bg-red-500"}
-              >
-                logout
-              </Button>{" "}
-              <p>{user.fullName}</p>
+              <section className="relative">
+                <button
+                  onClick={() => setToggle(!toggle)}
+                  className="cursor-pointer h-10 w-10 flex justify-center border-2 text-xl bg-purple-600 rounded-full"
+                >
+                  <p>{user.fullName.charAt(0)}</p>
+                </button>
+                <div
+                  className={`absolute ${!toggle && "hidden"} w-70 p-2 shaddow flex flex-col gap-10 py-5 bg-white text-black rounded-lg right-0 mt-5`}
+                >
+                  <section className="rounded-md h-fit flex flex-col items-center gap-3">
+                    <div className="rounded-[100%] w-20 h-20 flex overflow-hidden">
+                      <img
+                        src="/Rectangle3.png"
+                        alt=""
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                    <h1 className="font-semibold text-2xl line-clamp-1">
+                      {user.fullName}
+                    </h1>
+                    <p className="line-clamp-1 text-gray-400">{user.email}</p>
+                    <div className=" px-10 w-full text-center flex flex-col">
+                      <Link
+                      to={"/profile"}
+                        className="border-b-3 p-2 w-full border-gray-300 text-xl hover:border-primary"
+                      >
+                        Profile
+                      </Link>
+                      <Link
+                        to="/history"
+                        className="border-b-3 p-2 w-full border-gray-300 text-xl hover:border-primary"
+                      >
+                        History
+                      </Link>
+                    </div>
+                  </section>
+                  <button
+                    onClick={logout}
+                    className=" text-red-600 text-xl flex items-center gap-4 border-2 rounded-md hover:bg-red-600 hover:text-white p-1 w-full justify-center"
+                  >
+                    <p>Logout </p>
+                    <LogOut size={17} />
+                  </button>{" "}
+                </div>
+              </section>
             </div>
           ) : (
             <>
@@ -57,8 +107,8 @@ export default function Header() {
                 Sign in
               </Link>
               <Link
-                to="/register"
-                className="border border-primary bg-primary flex justify-center items-center rounded h-10 w-23"
+              to="/register"
+              className="border border-primary bg-primary flex justify-center items-center rounded h-10 w-23"
               >
                 Sign up
               </Link>
@@ -68,5 +118,6 @@ export default function Header() {
       </section>
       <MobileMenu onClick={toogleButton} toggle={toggle} />
     </nav>
+  
   );
 }
