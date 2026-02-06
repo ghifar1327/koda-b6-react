2ff808778c7c4e5c003749a4f67cb782c771b15b
ghifar1327 <givartorreto@gmail.com>
2026-02-07 03:32:59 +0700

    feat: create header for admin layout

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -11,24 +11,80 @@ export default function Header() {
   const location = useLocation();
 
   useEffect(() => {
-    (()=>{
+    (() => {
       setToggle(false);
-    })()
-}, [location.pathname]);
+    })();
+  }, [location.pathname]);
   function toogleButton(e) {
     e.preventDefault();
     setToggle((prev) => !prev);
   }
-  return (
-     user.role === "admin" ?
-      <nav 
-      className={`w-full z-1000 h-auto left-0 top-0  flex justify-between px-[5%] lg:px-[10%] p-3`}
-      >
-      </nav>
-      :
-      <nav
+  return user?.role === "admin" ? (
+    <nav
+      className={`w-full h-auto border-b-2 border-gray-300  flex justify-between px-[5%] p-3`}
+    >
+      <img src="/logos/coffiebrown.png" alt="" className="h-8" />
+      <section className="flex items-center gap-5">
+        <Link to="">
+          <Search size={26} className="text-gray-400" />
+        </Link>
+        <Link to={`${user ? "/payment" : "/login"}`}>
+          <ShoppingCart size={26} className="text-gray-400" />
+        </Link>
+        <div className="flex items-center gap-5">
+          <section className="relative">
+            <button
+              onClick={() => setToggle(!toggle)}
+              className="cursor-pointer h-10 w-10 flex justify-center border-2 text-xl bg-purple-600 rounded-full"
+            >
+              <p>{user.fullName.charAt(0)}</p>
+            </button>
+            <div
+              className={`absolute ${!toggle && "hidden"} w-70 p-2 shadow border-primary flex flex-col gap-10 py-5 bg-white text-black rounded-lg right-0 mt-5`}
+            >
+              <section className="rounded-md h-fit flex flex-col items-center gap-3">
+                <div className="rounded-[100%] w-20 h-20 flex overflow-hidden">
+                  <img
+                    src="/Rectangle3.png"
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <h1 className="font-semibold text-2xl line-clamp-1">
+                  {user.fullName}
+                </h1>
+                <p className="line-clamp-1 text-gray-400">{user.email}</p>
+                <div className=" px-10 w-full text-center flex flex-col">
+                  <Link
+                    to={"/profile"}
+                    className="border-b-3 p-2 w-full border-gray-300 text-xl hover:border-primary"
+                  >
+                    Profile
+                  </Link>
+                  <Link
+                    to="/history"
+                    className="border-b-3 p-2 w-full border-gray-300 text-xl hover:border-primary"
+                  >
+                    History
+                  </Link>
+                </div>
+              </section>
+              <button
+                onClick={logout}
+                className=" text-red-600 text-xl flex items-center gap-4 border-2 rounded-md hover:bg-red-600 hover:text-white p-1 w-full justify-center"
+              >
+                <p>Logout </p>
+                <LogOut size={17} />
+              </button>{" "}
+            </div>
+          </section>
+        </div>
+      </section>
+    </nav>
+  ) : (
+    <nav
       className={`w-full z-1000 h-auto left-0 top-0 ${location.pathname != "/" ? "bg-black relative" : "bg-[#0B090921] absolute"} text-white flex justify-between px-[5%] lg:px-[10%] p-3`}
-      >
+    >
       <section className="flex items-center gap-3 md:gap-10">
         <img src="/logos/coffieWhite.png" alt="" className="h-8" />
         <div className="hidden md:flex">
@@ -37,7 +93,7 @@ export default function Header() {
         <div className="hidden md:flex">
           <Link to="/product">Product</Link>
         </div>
-        </section>
+      </section>
       <section className="flex items-center gap-5">
         <Link to="">
           <Search size={26} />
@@ -59,7 +115,7 @@ export default function Header() {
                   <p>{user.fullName.charAt(0)}</p>
                 </button>
                 <div
-                  className={`absolute ${!toggle && "hidden"} w-70 p-2 shaddow flex flex-col gap-10 py-5 bg-white text-black rounded-lg right-0 mt-5`}
+                  className={`absolute ${!toggle && "hidden"} w-70 p-2 shadow flex flex-col gap-10 py-5 bg-white text-black rounded-lg right-0 mt-5`}
                 >
                   <section className="rounded-md h-fit flex flex-col items-center gap-3">
                     <div className="rounded-[100%] w-20 h-20 flex overflow-hidden">
@@ -75,7 +131,7 @@ export default function Header() {
                     <p className="line-clamp-1 text-gray-400">{user.email}</p>
                     <div className=" px-10 w-full text-center flex flex-col">
                       <Link
-                      to={"/profile"}
+                        to={"/profile"}
                         className="border-b-3 p-2 w-full border-gray-300 text-xl hover:border-primary"
                       >
                         Profile
@@ -107,8 +163,8 @@ export default function Header() {
                 Sign in
               </Link>
               <Link
-              to="/register"
-              className="border border-primary bg-primary flex justify-center items-center rounded h-10 w-23"
+                to="/register"
+                className="border border-primary bg-primary flex justify-center items-center rounded h-10 w-23"
               >
                 Sign up
               </Link>
@@ -118,6 +174,5 @@ export default function Header() {
       </section>
       <MobileMenu onClick={toogleButton} toggle={toggle} />
     </nav>
-  
   );
 }
