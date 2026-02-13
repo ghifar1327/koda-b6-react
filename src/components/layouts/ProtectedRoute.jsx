56b0a89589d6a07f66e93057ff19b1197782bfb7
ghifar1327 <givartorreto@gmail.com>
2026-02-13 15:20:32 +0700

    fix : Fixed bug by adding user validation so that it doesn't result in an error logging out from the admin page.

--- src/components/layouts/ProtectedRoute.jsx
+++ src/components/layouts/ProtectedRoute.jsx
@@ -5,6 +5,10 @@ import AuthContext from "../../context/AuthContext";
 export function ProtectedRoute({ children, role }) {
   const { user } = useContext(AuthContext);
 
+
+  if(!user){
+    return <Naviget to="/" replace/>
+  }
   
   if (role && user.role !== role) {
     return <Navigate to="/404" replace />;
