106c9b075b5fe35b732efe11feae07c80155447c
ghifar1327 <givartorreto@gmail.com>
2026-02-13 15:27:23 +0700

    feat: adds providor store from redux and persistor gete for rudux persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,22 @@ import { InvoiceProvider } from "./context/InvoiceContext.jsx";
 import { FetchProvider } from "./context/FetchContex.jsx";
 import Router from "./Router.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { Provider } from "react-redux";
+import {store,  persistor}  from "./redux/store.js"
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <AuthProvider>
-    <FetchProvider>
-      <InvoiceProvider>
-        <Router/>
-      </InvoiceProvider>
-    </FetchProvider>
-  </AuthProvider>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <AuthProvider>
+        <FetchProvider>
+          <InvoiceProvider>
+            <Router/>
+          </InvoiceProvider>
+        </FetchProvider>
+      </AuthProvider>
+    </PersistGate>
+  </Provider>
   // </StrictMode>,
 );
