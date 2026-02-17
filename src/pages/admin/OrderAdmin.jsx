775d9c7a2f23f1d5f2a39e8701cdbb591191062f
ghifar1327 <givartorreto@gmail.com>
2026-02-18 00:57:20 +0700

    feat: renders data for all product orders

--- src/pages/admin/OrderAdmin.jsx
+++ src/pages/admin/OrderAdmin.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
 import { Button } from '../../components/common/Button'
-import { Funnel, Search } from 'lucide-react'
+import { Funnel, PenLine, Search, Trash2 } from 'lucide-react'
+import useLocalStorage from '../../hooks/useLocalStotage'
 
 export default function OrderAdmin() {
+  const [users] =  useLocalStorage('users', null)
+  const historyOreder =users.filter(item => item.role === "user").flatMap(item => item.history)
+  console.log(historyOreder)
   return (
         <section className="p-[5%]">
-            <h1 className="text-5xl">Product List</h1>
+            <h1 className="text-5xl">Order List</h1>
             <div className="flex justify-between items-end">
               <Button onClick={(e)=> {e.preventDefault()}} size="w-fit p-2 h-fit px-3" orange>+ Add Order</Button>
               <div className="flex gap-2 ">
@@ -30,39 +33,42 @@ export default function OrderAdmin() {
             </div>
 
             {/* =================== Table */}
-            <section className="w-full mt-5 border rounded-xl border-gray-500 p-5">
+            <section className="w-full mt-5 border rounded-xl border-gray-500 p-3">
                <table className="w-full table-fixed">
-                 <thead>
+                 <thead className='w-full'>
                      <tr className="text-gray-500 font-normal">
-                     <th className="w-[5%] py-5">No. Order</th>
-                     <th className="w-[10%] py-5">Date</th>
+                     <th className="w-[10%] py-5">No. Order</th>
+                     <th className="w-[15%] py-5">Date</th>
                      <th className="w-[15%] py-5">Order</th>
                      <th className="w-[10%] py-5">Status</th>
                      <th className="w-[20%] py-5">Total</th>
                      <th className="w-[10%] py-5">Action</th>
                      </tr>
                  </thead>
-                 {/* <tbody className="text-center">
-                   {product.map((item, index) => (
-                     <tr key={item.productId} className={`${index %2 === 0 && "bg-gray-100"} text-gray-500`}>
-                       <td className="py-4">{index + 1}</td>
-                       <td className="py-4">
-                         <img
-                           src={item.images[0]}
-                           alt={item.name}
-                           className="w-12 h-12 mx-auto object-cover rounded"
-                         />
+                 <tbody className="text-center">
+                   {historyOreder.map((item, index) => (
+                     <tr key={item.id} className={`${index %2 === 0 && "bg-gray-100"} text-gray-500`}>
+                       <td className="py-4"> 
+                         #{item.id}
                        </td>
-                       <td className="py-4">{item.name}</td>
-                       <td className="py-4">IDR {item.price}</td>
-                       <td className="py-4 text-sm line-clamp-2">{item.description}
-                       </td>
-                       <td className="py-4">-</td>
-                       <td className="py-4">-</td>
-                       <td className="py-4">-</td>
+                       <td className="py-4">{
+                       new Date(item.create_at).toLocaleDateString("id-ID", {
+                            day: "2-digit",
+                            month: "long",
+                            year: "numeric"
+                          })}</td>
+                       <td>
+                          <ul className='w-full flex flex-col items-center'>
+                            {item.orders.map(item => {
+                              return <li className='list-disc w-fit'>{item.name}</li> 
+                            })}
+                          </ul>
+                        </td>
+                       <td className={`py-4 text-sm `}><p className={`${item.status == "On Progress" ? "bg-orange-200 text-orange-600" : item.status == "Pending"? "bg-red-200 text-red-600" : item.status == "Done"? "bg-green-200 text-green-600": "bg-gray-200 text-gray-600"} px-1 rounded-full`}></p>{item.status? item.status : "-" }</td>
+                       <td className="py-4">IDR {item.total}</td>
                        <td className="py-4">
                          <div className="flex justify-center gap-2">
-                           <button onClick={(e)=> {e.preventDefault(); setShowEditProduct(!showAddProduct)}} className="cursor-pointer w-8 h-8 bg-primary/30 text-primary flex items-center justify-center rounded-full">
+                           <button onClick={""} className="cursor-pointer w-8 h-8 bg-primary/30 text-primary flex items-center justify-center rounded-full">
                              <PenLine size={18} />
                            </button>
                            <button className="cursor-pointer w-8 h-8 bg-red-600/30 text-red-600 flex items-center justify-center rounded-full">
@@ -72,7 +78,7 @@ export default function OrderAdmin() {
                        </td>
                      </tr>
                    ))}
-                 </tbody> */}
+                 </tbody>
                 </table>
               </section>
         </section>
