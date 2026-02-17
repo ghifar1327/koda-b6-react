347667d5def2753b81813ae95cb87feffa86584e
ghifar1327 <givartorreto@gmail.com>
2026-02-18 01:37:29 +0700

    feat: adds empty cart layout when there are no products ordered

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,23 +1,32 @@
+import { TbPaperBagOff } from "react-icons/tb"; 
 import { Mail, MapPin, User, XCircle } from "lucide-react";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { Button } from "../components/common/Button";
 import Input from "../components/common/Input";
 import InvoiceContext from "../context/InvoiceContext";
 import AuthContext from "../context/AuthContext";
+import { nanoid } from "nanoid";
 
 export default function Payment() {
   const [delivery, setDelivery] = useState("");
   const { user } = useContext(AuthContext);
-  const [form, setForm] = useState({
-    email: user?.email || "",
-    fullName: user?.fullName || "",
-    address: user?.address || "",
-    delivery: "",
-  });
+  const [form, setForm] = useState({});
+  useEffect(() => {
+    (()=>{
+      if (user) {
+      setForm({
+        email: user.email || "",
+        fullName: user.fullName || "",
+        address: user.address || "",
+        delivery: "",
+      })
+    }
+    })();
+}, [user]);
+  const submitRef = useRef(null)
   const { cart, removeCart, setHistory } = useContext(InvoiceContext);
   const subtotal = cart.reduce((acc, item) => acc + Number(item.total), 0);
   const deliveryFee = delivery === "Door Delivery" ? 10000 : 0;
-
   const tax = (subtotal + deliveryFee) * 0.05;
 
   const grandTotal = subtotal + deliveryFee + tax;
@@ -28,16 +37,18 @@ export default function Payment() {
       [e.target.name]: e.target.value,
     }));
   }
-
+  console.log(cart.length)
   function handleSubmit() {
+    // e.preventDefault()
     if (!form.email || !form.fullName || !delivery) return;
 
     const data = {
-      id: Date.now(),
+      id: nanoid(10),
       fullName: form.fullName,
       email: form.email,
       address: form.address,
       delivery: delivery,
+      status : "On Progress",
       total: subtotal,
       orders: cart,
       create_at: Date.now(),
@@ -47,7 +58,17 @@ export default function Payment() {
   return (
     <div className="grid md:grid-cols-5 gap-10">
       <section className="order-1 md:order-1 flex flex-col gap-5 md:col-span-3 w-full">
-        {cart.slice(0, 4).map((item) => {
+        <div className="flex items-center justify-between">
+          <p className="text-4xl">Your order</p>
+          <Button orange size="w-fitt p-2 px-5">+ Add menu</Button>
+        </div>
+        {cart.length === 0 ? 
+            <div className="flex flex-col items-center justify-center gap-5 h-full   py-10 rounded">
+              <TbPaperBagOff size={50} className="text-gray-400"/>
+              <p className="text-xl font-semibold">Your cart is empty ☕</p>
+              <p className="text-gray-400">Please add some products first</p>
+          </div> :
+        cart.slice(0, 4).map((item) => {
           return (
             <>
               <div
@@ -89,8 +110,11 @@ export default function Payment() {
               </div>
             </>
           );
-        })}
+        })
+      }
       </section>
+      <div className="flex flex-col gap-5 order-3 md:order-2 md:col-span-2 h-fit">
+        <p className="text-3xl">Total</p>
       <section className="flex flex-col font-semibold text-xl gap-5 order-3 md:order-2 md:col-span-2 h-fit rounded w-full bg-[#E8E8E84D] p-5">
         <div className=" flex justify-between items-center">
           <p>Order</p>
@@ -104,16 +128,14 @@ export default function Payment() {
           <p>Tax</p>
           <p>Idr. {tax}</p>
         </div>
-        <div className="w-full h-[1px] bg-gray-400"></div>
+        <div className="w-full bg-gray-400"></div>
         <div className=" flex justify-between items-center">
           <p>Sub Total</p>
           <p>Idr. {grandTotal}</p>
         </div>
-        <form onSubmit={handleSubmit} className="font-normal">
-          <Button type="submit" orange>
+          <Button onClick={()=> submitRef.current?.click()} type="button" orange>
             Checkout
           </Button>
-        </form>
         <p className="text-gray-400 font-normal text-lg">We Accept</p>
         <div className="flex justify-between items-center">
           <img src="/eWallet/bri.svg" alt="BRI" className="w-[10%]" />
@@ -127,26 +149,27 @@ export default function Payment() {
           *Get Discount if you pay with Bank Central Asia
         </p>
       </section>
-      <section className="order-2 md:order-3 md:col-span-3 w-full flex flex-col gap-5">
+      </div>
+      <form  onSubmit={handleSubmit} className="order-2 md:order-3 md:col-span-3 w-full flex flex-col gap-5">
         <h1 className="text-3xl font-semibold">Payment Info & Deliver</h1>
         <Input
+          name="email"
           label="Email"
           type="email"
           id="email"
           value={form.email}
           onChange={handleChange}
-          defaultValue={user.email}
           placeholder="Enter Your Email"
         >
           <Mail size={18} />
         </Input>
         <Input
+        name="fullName"
           label="Full Name"
           type="text"
           id="fullname"
           value={form.fullName}
           onChange={handleChange}
-          defaultValue={user.fullName}
           placeholder="Enter Your Full Name"
         >
           <User size={18} />
@@ -155,9 +178,9 @@ export default function Payment() {
           label="Address"
           type="text"
           id="address"
+          name="address"
           value={form.address}
           onChange={handleChange}
-          defaultValue={"karawang"}
           placeholder="Enter Your Address"
         >
           <MapPin size={18} />
@@ -199,7 +222,8 @@ export default function Payment() {
             </Input>
           </div>
         </div>
-      </section>
+        <button ref={submitRef} type="submit" className="hidden"></button>
+      </form>
     </div>
   );
 }
