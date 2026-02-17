bfd08abffcba15f7b98c1f3cb8098833f90b552c
ghifar1327 <givartorreto@gmail.com>
2026-02-17 11:06:25 +0700

    feat: adds product list page for admin

---
+++ src/pages/admin/OrderAdmin.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Button } from '../../components/common/Button'
+import { Funnel, Search } from 'lucide-react'
+
+export default function OrderAdmin() {
+  return (
+        <section className="p-[5%]">
+            <h1 className="text-5xl">Product List</h1>
+            <div className="flex justify-between items-end">
+              <Button onClick={(e)=> {e.preventDefault()}} size="w-fit p-2 h-fit px-3" orange>+ Add Order</Button>
+              <div className="flex gap-2 ">
+                  <div className='w-fit'>
+                      <p className="mb-2 text-gray-400">Status</p>
+                      <div className='pr-3 rounded border border-gray-200'>
+                      <select className="p-2.5  flex items-center w-70 outline-none">
+                         <option value={"all"}>All</option>
+                      </select>
+                      </div>
+                  </div>
+                  <label htmlFor="" className="flex items-end gap-3">
+                    <div>
+                        <p className="mb-2 text-gray-400">search product</p>
+                        <div className="p-2 px-3 rounded border border-gray-200 flex items-center w-fit">
+                        <input className="outline-none"/><Search/>
+                        </div>
+                    </div>
+                    <Button orange size="w-fit h-fit p-2 px-3"><Funnel/> Filter</Button>  
+                  </label>
+              </div>
+            </div>
+
+            {/* =================== Table */}
+            <section className="w-full mt-5 border rounded-xl border-gray-500 p-5">
+               <table className="w-full table-fixed">
+                 <thead>
+                     <tr className="text-gray-500 font-normal">
+                     <th className="w-[5%] py-5">No. Order</th>
+                     <th className="w-[10%] py-5">Date</th>
+                     <th className="w-[15%] py-5">Order</th>
+                     <th className="w-[10%] py-5">Status</th>
+                     <th className="w-[20%] py-5">Total</th>
+                     <th className="w-[10%] py-5">Action</th>
+                     </tr>
+                 </thead>
+                 {/* <tbody className="text-center">
+                   {product.map((item, index) => (
+                     <tr key={item.productId} className={`${index %2 === 0 && "bg-gray-100"} text-gray-500`}>
+                       <td className="py-4">{index + 1}</td>
+                       <td className="py-4">
+                         <img
+                           src={item.images[0]}
+                           alt={item.productName}
+                           className="w-12 h-12 mx-auto object-cover rounded"
+                         />
+                       </td>
+                       <td className="py-4">{item.productName}</td>
+                       <td className="py-4">IDR {item.price}</td>
+                       <td className="py-4 text-sm line-clamp-2">{item.description}
+                       </td>
+                       <td className="py-4">-</td>
+                       <td className="py-4">-</td>
+                       <td className="py-4">-</td>
+                       <td className="py-4">
+                         <div className="flex justify-center gap-2">
+                           <button onClick={(e)=> {e.preventDefault(); setShowEditProduct(!showAddProduct)}} className="cursor-pointer w-8 h-8 bg-primary/30 text-primary flex items-center justify-center rounded-full">
+                             <PenLine size={18} />
+                           </button>
+                           <button className="cursor-pointer w-8 h-8 bg-red-600/30 text-red-600 flex items-center justify-center rounded-full">
+                             <Trash2 size={18} />
+                           </button>
+                         </div>
+                       </td>
+                     </tr>
+                   ))}
+                 </tbody> */}
+                </table>
+              </section>
+        </section>
+  )
+}
