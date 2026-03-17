43e1861281ca26c46afb5315401b7d875287f850
ghifar1327 <givartorreto@gmail.com>
2026-03-18 00:25:16 +0700

    fix: change async function

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,15 +1,19 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL || "https://ghifar-backend.camps.fahrul.id"
+// const BASE_URL = import.meta.env.VITE_BASE_URL || "https://ghifar-backend.camps.fahrul.id"
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8888"
 
-function http(url, body ,opts={}){
+
+async function http(url, body ,opts={}){
     const headers = {}
     if(opts.token){
         headers.Authorization = "Bearer "+opts.token
     }
-    return fetch(BASE_URL + url, {
+    const res = await fetch(BASE_URL + url, {
         method: opts.method || "GET",
         body,
         headers
     })
+    const data = await res.json()
+    return data
 }
 
 export default http;
