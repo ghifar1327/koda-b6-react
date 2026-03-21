656c0c6aa3c946f283950194bc05adafe8ce3d6f
ghifar1327 <givartorreto@gmail.com>
2026-03-21 15:20:55 +0700

    feat: add modal to display response from backend

--- src/pages/ForgotPwd.jsx
+++ src/pages/ForgotPwd.jsx
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Input from "../components/common/Input";
 import { Button } from "../components/common/Button";
 import { Mail } from "lucide-react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup"
 import { yupResolver } from "@hookform/resolvers/yup";
+import Modal from "../components/feature/Modal";
+import { useNavigate } from "react-router";
+import AuthContext from "../context/AuthContext";
 
 const scehema = yup.object({
   email : yup.string().email("Invalid Format Email").required("Email must be filled in")
 })
 export default function ForgotPwd() {
+  const [count, setCount] = useState(5);
+  const navigate = useNavigate();
   const {handleSubmit, register, reset , formState : {errors}} = useForm({
     resolver : yupResolver(scehema)
   })
- function action(form){
+  const { forgotPassword, error, setError, isSuccess, setIsSuccess, message } = useContext(AuthContext);
+ 
+ useEffect(() => {
+    setIsSuccess(false);
+ }, []);
+ useEffect(() => {
+     if (isSuccess) {
+       setError(false);
+       reset();
+       const timer = setInterval(() => {
+         setCount((prev) => {
+           if (prev <= 1) {
+             clearInterval(timer);
+             navigate("/reset-password")
+               return 0;
+             }
+           return prev - 1;
+         });
+       }, 1000);
+     }
+   }, [isSuccess]);
+ 
+ 
+ 
+ 
+  function action(form){
   console.log(form)
+  forgotPassword(form)
   reset()
  }
   return (
@@ -40,6 +71,31 @@ export default function ForgotPwd() {
         <span className="text-red-500">{errors.email?.message}</span>
         <Button orange>Submit</Button>
       </form>
+
+
+      
+      {/* ================================== Modal ===================================*/}
+      
+      
+      <Modal success={isSuccess}>
+        <p className="text-3xl text-gray-700">{message}</p>
+        <div className="flex flex-col w-full gap-2">
+          <Button orange onClick={() => setCount(0)}>
+            Enter
+          </Button>
+          <p className="text-gray-500 text-center">Redirect in {count}s</p>
+        </div>
+      </Modal>
+      <Modal error={error} onClick={() => setError(!error)}>
+        <p className="text-2xl text-gray-700 w-70 text-center">
+          {message}
+        </p>
+        <div className="flex flex-col gap-3 w-full text-center">
+          <Button orange onClick={() => setError(!error)}>
+            Try Again
+          </Button>
+        </div>
+      </Modal>
       </>
     // </AuthLayout>
   );
