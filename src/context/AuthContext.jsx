04441fe1aace0c18bbe670f28f296c731bb8c348
ghifar1327 <givartorreto@gmail.com>
2026-03-23 22:57:07 +0700

    fix: logic auth login function

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,41 +1,22 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import useLocalStorage from "../hooks/useLocalStotage";
-import { nanoid } from "nanoid";
 import http from "../lib/http";
 
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
-  const adminAccount = {
-    id: nanoid(10),
-    image: "",
-    fullName: "koda",
-    email: "admin@mail.com",
-    phone: "085591710309",
-    address: "Depok",
-    role: "admin",
-    password: "1234A",
-  };
+
 
   const [user, setUser] = useLocalStorage("user", null);
-  const [users, setUsers] = useLocalStorage("users", []);
   const [error, setError] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
   const [message, setMessage] = useState("")
 
-  useEffect(() => {
-    if (users.length === 0) {
-      setUsers([adminAccount]);
-    }
-  }, []);
 
 async function login(form) {
     const data = {
-    address: form.address,
     email: form.email,
-    full_name: form.fullName,
     password: form.password,
-    phone: String(form.phone)
   }
   
   try {
@@ -44,8 +25,8 @@ async function login(form) {
     if (!res.success) {
       throw new Error(res.message)
     }
-    // console.log(res.user)
-    setUser(res.user)
+    console.log(res)
+    setUser(res)
     setError(false)
     setIsSuccess(true)
     setMessage(res.message)
@@ -127,25 +108,6 @@ console.log(req)
   }  
 }
 
-function updateProfile(updatedData) {
-  if (!user) return { success: false, message: "User not logged in" };
-
-  // update list users
-  const updatedUsers = users.map((u) =>
-    u.id === user.id ? { ...u, ...updatedData } : u
-  );
-
-  setUsers(updatedUsers);
-
-  // update user yang sedang login
-  const newUserData = { ...user, ...updatedData };
-  setUser(newUserData);
-
-  setIsSuccess(true);
-  setError(false);
-
-  return { success: true };
-}
 
   function logout() {
     localStorage.removeItem("user");
@@ -159,13 +121,11 @@ function updateProfile(updatedData) {
     <AuthContext.Provider
       value={{
         user,
-        users,
         login,
         registerUser,
         logout,
         forgotPassword,
         resetPassword,
-        updateProfile,
         error,
         setError,
         isSuccess,
