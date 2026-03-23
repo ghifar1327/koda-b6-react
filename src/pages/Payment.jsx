7f996a585d4945b070bb5dd1538a5f633c80644f
ghifar1327 <givartorreto@gmail.com>
2026-03-23 23:03:00 +0700

    feat(payment): fetch delivery methods and fix pricing calculation

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -6,27 +6,43 @@ import Input from "../components/common/Input";
 import InvoiceContext from "../context/InvoiceContext";
 import AuthContext from "../context/AuthContext";
 import { nanoid } from "nanoid";
+import http from "../lib/http";
 
 export default function Payment() {
-  const [delivery, setDelivery] = useState("");
+const [methods, setMethods] = useState([]); // dari API
+const [selectedDelivery, setSelectedDelivery] = useState(0);
   const { user } = useContext(AuthContext);
   const [form, setForm] = useState({});
   useEffect(() => {
     (()=>{
       if (user) {
       setForm({
-        email: user.email || "",
-        fullName: user.fullName || "",
-        address: user.address || "",
-        delivery: "",
+        email: user.user.email || "",
+        fullName: user.user.full_name || "",
+        // address: user.address || "",
+        // delivery: "",
       })
     }
     })();
 }, [user]);
+
+useEffect(()=>{
+  (async ()=>{
+    try{
+      const res = await http("/master/methods");
+      setMethods(res.results);
+      console.loq(res.results)
+    }catch(err){
+      return err
+    }
+  })()
+},[])
   const submitRef = useRef(null)
   const { cart, removeCart, setHistory } = useContext(InvoiceContext);
-  const subtotal = cart.reduce((acc, item) => acc + Number(item.total), 0);
-  const deliveryFee = delivery === "Door Delivery" ? 10000 : 0;
+  const subtotal = cart.reduce((acc, item) => acc + Number(item.subtotal), 0);
+  const selectedMethod = methods.find(item => item.id === selectedDelivery);
+  
+  const deliveryFee = selectedMethod?.add_price || 0;
   const tax = (subtotal + deliveryFee) * 0.05;
 
   const grandTotal = subtotal + deliveryFee + tax;
@@ -40,7 +56,7 @@ export default function Payment() {
   console.log(cart.length)
   function handleSubmit() {
     // e.preventDefault()
-    if (!form.email || !form.fullName || !delivery) return;
+    if (!form.email || !form.fullName || !selectedDelivery) return;
 
     const data = {
       id: nanoid(10),
@@ -48,7 +64,7 @@ export default function Payment() {
       email: form.email,
       phone: user.phone,
       address: form.address,
-      delivery: delivery,
+      delivery: selectedDelivery,
       status : "On Progress",
       total: subtotal,
       orders: cart,
@@ -56,6 +72,8 @@ export default function Payment() {
     };
     setHistory(data);
   }
+
+  console.log(selectedMethod?.name)
   return (
     <div className="grid md:grid-cols-5 gap-10">
       <section className="order-1 md:order-1 flex flex-col gap-5 md:col-span-3 w-full">
@@ -77,11 +95,11 @@ export default function Payment() {
                 className="bg-[#E8E8E84D] w-full p-2 pr-5 rounded flex justify-between items-center gap-2"
               >
                 <div className="flex gap-2">
-                  <div className="w-[20%]">
+                  <div className="w-[20%] h-[full] aspect-square overflow-hidden">
                     <img
                       src={item.image}
                       alt={item.name}
-                      className="object-center"
+                      className="object-cover"
                     />
                   </div>
                   <div className="flex flex-col justify-between">
@@ -92,15 +110,15 @@ export default function Payment() {
                       {item.name}
                     </p>
                     <p className="text-xl xl:text-xl text-gray-400">
-                      {item.quantity}pcs | {item.size} | {item.temperature} |{" "}
-                      {delivery}
+                      {item.quantity}pcs | {item.size.name} | {item.variant.name} |{" "}
+                      {selectedMethod?.name}
                     </p>
                     <div className="flex items-center gap-2">
-                      <p className="text-xs text-red-600 line-through">
-                        IDR {item.price}
-                      </p>
+                      {/* <p className="text-xs text-red-600 line-through">
+                        IDR {""}
+                      </p> */}
                       <p className="text-xl xl:text-xl text-primary">
-                        IDR {item.total}
+                        IDR {item.price?.toLocaleString("id-ID")}
                       </p>
                     </div>
                   </div>
@@ -119,20 +137,20 @@ export default function Payment() {
       <section className="flex flex-col font-semibold text-xl gap-5 order-3 md:order-2 md:col-span-2 h-fit rounded w-full bg-[#E8E8E84D] p-5">
         <div className=" flex justify-between items-center">
           <p>Order</p>
-          <p>Idr. {subtotal}</p>
+          <p>Idr. {subtotal?.toLocaleString("id-ID")}</p>
         </div>
         <div className=" flex justify-between items-center">
           <p>Delivery</p>
-          <p>Idr. {deliveryFee}</p>
+          <p>Idr. {deliveryFee?.toLocaleString("id-ID")}</p>
         </div>
         <div className=" flex justify-between items-center">
           <p>Tax</p>
-          <p>Idr. {tax}</p>
+          <p>Idr. {tax?.toLocaleString("id-ID")}</p>
         </div>
         <div className="w-full bg-gray-400"></div>
         <div className=" flex justify-between items-center">
           <p>Sub Total</p>
-          <p>Idr. {grandTotal}</p>
+          <p>Idr. {grandTotal?.toLocaleString("id-ID")}</p>
         </div>
           <Button onClick={()=> submitRef.current?.click()} type="button" orange>
             Checkout
@@ -191,7 +209,19 @@ export default function Payment() {
             Delivery
           </label>
           <div className="flex gap-7">
+            {methods.map((item) => (
             <Input
+              key={item.id}
+              type="radio"
+              name="delivery"
+              value={item.id}
+              onChange={(e) => setSelectedDelivery(Number(e.target.value))}
+            >
+              {item.name}
+            </Input>
+          ))}
+            
+            {/* <Input
               type="radio"
               name="delivery"
               value="Dine In"
@@ -220,7 +250,7 @@ export default function Payment() {
               onChange={(e) => setDelivery(e.target.value)}
             >
               Pick Up
-            </Input>
+            </Input> */}
           </div>
         </div>
         <button ref={submitRef} type="submit" className="hidden"></button>
