d7c518ccf73443d6a727b067c813a1ea97334798
ghifar1327 <givartorreto@gmail.com>
2026-03-27 03:41:25 +0700

    fix: headers request

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,23 +1,38 @@
+
 // const BASE_URL = import.meta.env.VITE_BASE_URL || "https://ghifar-backend.camps.fahrul.id"
 const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8888"
 
 
-async function http(url, body ,opts={}){
-    const headers = {}
-    if(opts.token){
-        headers.Authorization = "Bearer "+opts.token
-    }
-    const res = await fetch(BASE_URL + url, {
+async function http(url, body, opts = {}) {
+  const token = localStorage.getItem("token")
+  
+  const headers = {
+    "Content-Type": "application/json",
+    ...(token && token.split(".").length === 3 && {
+      Authorization: "Bearer " + token,
+    }),
+  }
+
+  const res = await fetch(BASE_URL + url, {
     method: opts.method || "GET",
-    body,
-    headers
-    })
-  
-    if (!res.ok) {
-      throw new Error("Failed to fetch")
-    }
-  
-    return res.json()
+    body: opts.method === "GET" ? undefined : body,
+    headers,
+  })
+
+  const data = await res.json()
+
+    if (res.status === 401) {
+    // hapus token lama
+    localStorage.removeItem("token")
+    // arahkan ke login
+    window.location.href = "/login"
+    return
+  }
+  if (!res.ok) {
+    throw new Error(data.message || "Failed to fetch")
+  }
+
+  return data
 }
 
 export default http;
