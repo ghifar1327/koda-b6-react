4c6f3f59d94b31ec81017cd23fe7c39e48166266
ghifar1327 <givartorreto@gmail.com>
2026-03-27 00:50:42 +0700

    feat: add func update profile for req api , token set item local storage

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,106 +7,127 @@ const AuthContext = createContext(null);
 export function AuthProvider({ children }) {
 
 
-  const [user, setUser] = useLocalStorage("user", null);
-  const [error, setError] = useState(false);
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [message, setMessage] = useState("")
+     const [user, setUser] = useLocalStorage("user", null);
+     const [error, setError] = useState(false);
+     const [isSuccess, setIsSuccess] = useState(false);
+     const [message, setMessage] = useState("")
+   
+   
+   async function login(form) {
+       const data = {
+       email: form.email,
+       password: form.password,
+     }
+     
+     try {
+       const res = await http("/auth/login", JSON.stringify(data),{method: "POST"})
+   
+       if (!res.success) {
+         throw new Error(res.message)
+       }
+       console.log(res)
+       setUser(res)
+       localStorage.setItem("token", res.token)
+       setError(false)
+       setIsSuccess(true)
+       setMessage(res.message)
+   
+     } catch (err) {
+       setError(true)
+       setIsSuccess(false)
+       setMessage(err.message || "Someting is Wrong")
+     }
+     }
+   
+   async function registerUser(form) {
+     const data = {
+       address: form.address,
+       email: form.email,
+       full_name: form.fullName,
+       password: form.password,
+       phone: String(form.phone)
+     }
+     
+     try {
+       const res = await http("/auth/register", JSON.stringify(data),{method: "POST"})
+   
+       if (!res.success) {
+         throw new Error(res.message)
+       }
+   
+       setError(false)
+       setIsSuccess(true)
+       setMessage(res.message)
+   
+     } catch (err) {
+       setError(true)
+       setIsSuccess(false)
+       setMessage(err.message || "Someting is Wrong")
+     }
+   }
 
-
-async function login(form) {
-    const data = {
-    email: form.email,
-    password: form.password,
-  }
-  
-  try {
-    const res = await http("/auth/login", JSON.stringify(data),{method: "POST"})
-
-    if (!res.success) {
-      throw new Error(res.message)
-    }
-    console.log(res)
-    setUser(res)
-    setError(false)
-    setIsSuccess(true)
-    setMessage(res.message)
-
-  } catch (err) {
-    setError(true)
-    setIsSuccess(false)
-    setMessage(err.message || "Someting is Wrong")
-  }
-  }
-
-async function registerUser(form) {
-  const data = {
-    address: form.address,
-    email: form.email,
-    full_name: form.fullName,
-    password: form.password,
-    phone: String(form.phone)
-  }
-  
-  try {
-    const res = await http("/auth/register", JSON.stringify(data),{method: "POST"})
-
-    if (!res.success) {
-      throw new Error(res.message)
-    }
-
-    setError(false)
-    setIsSuccess(true)
-    setMessage(res.message)
-
-  } catch (err) {
-    setError(true)
-    setIsSuccess(false)
-    setMessage(err.message || "Someting is Wrong")
-  }
-}
+   async function updateProfile(data, id){
+    try {
+       const res = await http(`/auth/${id}/update`, JSON.stringify(data),{method: "PATCH"})
+   
+       if (!res.success) {
+         throw new Error(res.message)
+       }
+   
+       setError(false)
+       setIsSuccess(true)
+       setMessage(res.message)
+   
+     } catch (err) {
+       setError(true)
+       setIsSuccess(false)
+       setMessage(err.message || "Someting is Wrong")
+     }
+   }
+   
 
 // =============================================================================== reqeust FG
 
-async function forgotPassword(email){
-  try{
-    const res = await http("/auth/forgot-password", JSON.stringify(email),{method: "POST"})
-    if (!res.success) {
-      throw new Error(res.message)
+   async function forgotPassword(email){
+     try{
+       const res = await http("/auth/forgot-password", JSON.stringify(email),{method: "POST"})
+       if (!res.success) {
+         throw new Error(res.message)
+       }   
+
+       setError(false)
+       setIsSuccess(true)
+       setMessage(res.message)
+     }catch(err){
+       setError(true)
+       setIsSuccess(false)
+       setMessage(err.message || "Someting is Wrong")
+     }
     }
 
-    setError(false)
-    setIsSuccess(true)
-    setMessage(res.message)
-  }catch(err){
-    setError(true)
-    setIsSuccess(false)
-    setMessage(err.message || "Someting is Wrong")
-  }
-}
 
-
-async function resetPassword(data) {
-  const req = {
-  code: Number(data.otp),
-  email: data.email,
-  new_password: data.password
-}
-console.log(req)
-  try{
-    const res = await http("/auth/reset-password", JSON.stringify(req),{method: "PATCH"})
-    if (!res.success) {
-      throw new Error(res.message)
-    }
-
-    setError(false)
-    setIsSuccess(true)
-    setMessage(res.message)
-  }catch(err){
-    setError(true)
-    setIsSuccess(false)
-    setMessage(err.message || "Someting is Wrong")
-  }  
-}
+   async function resetPassword(data) {
+     const req = {
+     code: Number(data.otp),
+     email: data.email,
+     new_password: data.password
+   }
+   console.log(req)
+     try{
+       const res = await http("/auth/reset-password", JSON.stringify(req),{method: "PATCH"})
+       if (!res.success) {
+         throw new Error(res.message)
+       }
+   
+       setError(false)
+       setIsSuccess(true)
+       setMessage(res.message)
+     }catch(err){
+       setError(true)
+       setIsSuccess(false)
+       setMessage(err.message || "Someting is Wrong")
+     }  
+   }
 
 
   function logout() {
@@ -124,6 +145,7 @@ console.log(req)
         login,
         registerUser,
         logout,
+        updateProfile,
         forgotPassword,
         resetPassword,
         error,
