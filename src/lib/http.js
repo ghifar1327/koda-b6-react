d78674c44c35590d482230db916bf580fed32ebc
ghifar1327 <givartorreto@gmail.com>
2026-04-04 03:18:05 +0700

    fix(http): improve headers handling for json and form-data requests

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,38 +1,44 @@
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || "https://ghifar-backend.camps.fahrul.id"
-// const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8888"
+// const BASE_URL = import.meta.env.VITE_BASE_URL || "https://ghifar-backend.camps.fahrul.id"
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8888"
 
 
 async function http(url, body, opts = {}) {
-  const token = localStorage.getItem("token")
-  
+  const token = localStorage.getItem("token");
+
   const headers = {
-    "Content-Type": "application/json",
+    ...(opts.isForm ? {} : { "Content-Type": "application/json" }),
     ...(token && token.split(".").length === 3 && {
       Authorization: "Bearer " + token,
     }),
-  }
+  };
 
   const res = await fetch(BASE_URL + url, {
     method: opts.method || "GET",
     body: opts.method === "GET" ? undefined : body,
     headers,
-  })
+  });
 
-  const data = await res.json()
+  const text = await res.text();
 
-    if (res.status === 401) {
-    // hapus token lama
-    localStorage.removeItem("token")
-    // arahkan ke login
-    window.location.href = "/login"
-    return
+  let data = null;
+  try {
+    data = text ? JSON.parse(text) : null;
+  } catch (err) {
+    return err
+    // console.error("Response bukan JSON:", text);
   }
+
+  if (res.status === 401) {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+    return;
+  }
+
   if (!res.ok) {
-    throw new Error(data.message || "Failed to fetch")
+    throw new Error(data?.message || "Failed to fetch");
   }
 
-  return data
+  return data;
 }
-
 export default http;
