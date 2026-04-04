acce1770b80d5ddc7f454768a65d8491b0038b16
ghifar1327 <givartorreto@gmail.com>
2026-04-05 04:05:04 +0700

    separate the handles for submitting data updates and uploading pictures

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,12 +6,10 @@ import Header from "../components/layouts/Header";
 import AuthContext from "../context/AuthContext";
 import { useForm } from "react-hook-form";
 import Modal from "../components/feature/Modal";
-import { Link, useNavigate } from "react-router";
 
 export default function Profile() {
   const [file, setFile] = useState(null);
-  const navigate = useNavigate()
-  const{user, updateProfile, isSuccess, setIsSuccess, error, setError, message, logout}= useContext(AuthContext)
+  const{user, updateProfile, updatePicture, isSuccess, setIsSuccess, error, setError, message}= useContext(AuthContext)
   const{register, handleSubmit} = useForm()
   // console.log(isSuccess)
 
@@ -21,23 +19,23 @@ export default function Profile() {
   }, [])
 function action(form){
   // console.log(form)
-  updateProfile(form, user.user.id, file)
+  updateProfile(form, user.id)
 }
   return (
      <>
       <Header/>
       <main className="p-[5%]">
           <h1 className="text-5xl font-semibold mb-6">Profile</h1>
-          <form className="flex flex-col md:flex-row md:gap-5 gap-10 lg:gap-10 w-full" onSubmit={handleSubmit(action)}>
-            <section className="flex-1 border-2 rounded border-gray-300 p-3 w-full h-fit flex flex-col items-center gap-3">
-                <h1 className="font-semibold text-2xl">{user.user.full_name}</h1>
-                <p className="text-[#4F5665]">{user.user.email}</p>
+           <div className="flex flex-col md:flex-row md:gap-5 gap-10 lg:gap-10 w-full" >
+            <form className="flex-1 border-2 rounded border-gray-300 p-3 w-full h-fit flex flex-col items-center gap-3">
+                <h1 className="font-semibold text-2xl">{user?.full_name}</h1>
+                <p className="text-[#4F5665]">{user?.email}</p>
                 <div className="rounded-[100%] w-50 h-50 overflow-hidden">
                   <img
                     src={
                       file
-                        ? URL.createObjectURL(file) // preview
-                        : user?.user.picture || "/Rectangle3.png"
+                        ? URL.createObjectURL(file)
+                        : user?.picture || "/Rectangle3.png"
                     }
                     alt=""
                     className="w-full h-full object-cover"
@@ -47,7 +45,13 @@ function action(form){
                 <input
                   type="file"
                   accept="image/*"
-                  onChange={(e) => {setFile(e.target.files[0]); console.log("TERPANGGIL");}}
+                    onChange={(e) => {
+                      const selected = e.target.files[0];
+                      setFile(selected);
+                      if (selected) {
+                        updatePicture(selected, user.id);
+                      }
+                    }}
                   className="hidden"
                   id="upload"
                 />
@@ -56,28 +60,23 @@ function action(form){
                     add new picture
                 </label>
                 <p className="font-semibold text-[#4F5665]"><span className="font-normal">Since</span> 20 January 2022</p>
-            </section>
-            <div className="border border-gray-300 flex-2/5 h-fit rounded p-3 flex flex-col gap-3" >
-                <Input label="Full Name" type="text" id="fullName" defaultValue={user?.user.full_name} {...register("full_name")}><User size={18}/></Input>
-                <Input label="Email" type="email" id="email"defaultValue={user?.user.email} {...register("email")}><Mail size={18}/></Input>
-                <Input label="Phone" type="number" id="phone" defaultValue={user?.user.phone} {...register("phone")}><PhoneCall size={18}/></Input>
-                {/* <label htmlFor="password" className="font-semibold flex justify-between -mb-3">
-                    <p>Password</p>
-                    <p className="text-primary font-normal">Set New Password</p>
-                </label>
-                <Input label="" type="password" id="password" password><KeyRound size={18}/></Input> */}
-                <Input label="Address" type="text" id="address" defaultValue={user?.user.address} {...register("address")}><MapPin size={18}/></Input>
+            </form>
+            <form className="border border-gray-300 flex-2/5 h-fit rounded p-3 flex flex-col gap-3" onSubmit={handleSubmit(action)}>
+                <Input label="Full Name" type="text" id="fullName" defaultValue={user?.full_name} {...register("full_name")}><User size={18}/></Input>
+                <Input label="Email" type="email" id="email"defaultValue={user?.email} {...register("email")}><Mail size={18}/></Input>
+                <Input label="Phone" type="number" id="phone" defaultValue={user?.phone} {...register("phone")}><PhoneCall size={18}/></Input>
+                <Input label="Address" type="text" id="address" defaultValue={user?.address} {...register("address")}><MapPin size={18}/></Input>
                 <Button orange>Submit</Button>
-            </div>
-          </form>
+            </form>
+          </div>
       </main>
       <Modal success={isSuccess} onClick={()=> setIsSuccess(!isSuccess)}>
         <p className="text-3xl text-gray-700">{message}</p>
         <Button orange
-          onClick={()=> {setIsSuccess(!isSuccess); logout(); navigate("/login")}}
+          onClick={()=> {setIsSuccess(!isSuccess)}}
           className="bg-primary p-2 rounded-md w-full text-center"
         >
-          Please login again
+          OK
         </Button>
       </Modal>
       <Modal error={error} onClick={() => setError(!error)}>
