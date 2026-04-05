8b11ff7c9c8c79f98ccfa3e939a8349175ec0a7a
ghifar1327 <givartorreto@gmail.com>
2026-04-05 23:21:16 +0700

    chore: fix base_url

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,6 +1,5 @@
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || "https://ghifar-backend.camps.fahrul.id"
-// const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8888"
+const BASE_URL = import.meta.env.VITE_BASE_URL || import.meta.env.VITE_BASE_URL
 async function http(url, body, opts = {}) {
   const token = localStorage.getItem("token");
 
